a2b84d3c259dd655bd930d241091e32f7aa7262a
MouhibBahri <123774260+MouhibBahri@users.noreply.github.com>
2024-03-25 21:02:21 +0100

    added to do list

--- navbar.php
+++ navbar.php
@@ -11,6 +11,10 @@
             width: 56px;
             height: 56px;
         }
+
+        .primary{
+            color: white !important;
+        }
         i{
             font-size: 18px;
         }
@@ -18,7 +22,33 @@
             width: 56px;
             z-index: 2;
         }
-    </style>
+        .tr {
+            background-color: transparent;
+            color: white;
+            border-bottom: 1px solid #BFA181 ;
+        }
+
+        .tr:nth-child(odd) {
+            background-color: #178582;
+        }
+        .custom-btn-close{--bs-btn-close-color: #000;
+            --bs-btn-close-bg: url("images/close_btn.svg");
+        --bs-btn-close-opacity: 0.5;
+        --bs-btn-close-hover-opacity: 0.75;
+        --bs-btn-close-focus-shadow: 0 0 0 0.25rem rgba(13, 110, 253, 0.25);
+        --bs-btn-close-focus-opacity: 1;
+        --bs-btn-close-disabled-opacity: 0.25;
+        --bs-btn-close-white-filter: invert(1) grayscale(100%) brightness(200%);
+        box-sizing: content-box;
+        width: 2em;
+        height:2em;
+        color: var(--bs-btn-close-color);
+        background: transparent var(--bs-btn-close-bg) center/1.2em auto no-repeat;
+        border: 0;
+        border-radius: .375rem;
+        opacity: var(--bs-btn-close-opacity);
+        }
+         </style>
     <title></title>
 </head>
 <body>
@@ -40,6 +70,25 @@
         <i class="bi bi-sliders2"></i>
     </button>
 </div>
+<?php
+// Check if the current page is not "index.php"
+if(basename($_SERVER['PHP_SELF']) != 'index.php') {
+    // Second part of the navbar (only shown on pages other than "index.php")
+    echo '<button style="z-index: 2" type="button" class="rounded-pill position-fixed bottom-0 end-0 mb-3 me-3 w-auto py-2 px-4" data-bs-toggle="offcanvas" data-bs-target="#todo-list" aria-controls="offcanvasWithBothOptions" >
+    To-Do List
+    <span class="badge text-bg-secondary">4</span>
+</button>
+
+<div class="offcanvas offcanvas-start" style="background-color: #061A26" data-bs-scroll="true" tabindex="-1" id="todo-list" aria-labelledby="offcanvasWithBothOptionsLabel">
+    <div class="offcanvas-header justify-content-between">
+        <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel" style="color: white">To-Do List</h5>
+        <button type="button" class="custom-btn-close" data-bs-dismiss="offcanvas"  aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">';
+    include_once "todolist.php";
+    echo '</div></div>';
+}
+?>
 
 </body>
 <script crossorigin="anonymous"
