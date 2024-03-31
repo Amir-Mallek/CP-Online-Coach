a3f5195bd28537b0631b325a34029beea07a6f44
MouhibBahri <123774260+MouhibBahri@users.noreply.github.com>
2024-03-31 05:21:42 +0100

    Add custom scrollbar

--- auto_load.php
+++ auto_load.php
@@ -7,6 +7,25 @@ function link_bootstrap_style() {
     echo '
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <style>
+    ::-webkit-scrollbar {
+    width: 20px;
+    }
+
+    ::-webkit-scrollbar-track {
+    background-color: transparent;
+    }
+
+    ::-webkit-scrollbar-thumb {
+    background-color: #53575a;
+    border-radius: 20px;
+    border: 6px solid transparent;
+    background-clip: content-box;
+    }
+
+    ::-webkit-scrollbar-thumb:hover {
+    background-color: #3f3f42;
+    }</style>
     ';
 }
 
