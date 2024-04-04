348af833b3c50e59dfac3d1e27960a176eb48b93
Amir-Mallek <123602395+Amir-Mallek@users.noreply.github.com>
2024-04-04 04:54:55 +0100

    Update auto_load.php

--- auto_load.php
+++ auto_load.php
@@ -37,7 +37,27 @@ function link_bootstrap_script() {
     echo '</script>';
 }
 
-function auto_loader($class_name) {
-    require_once "includes/$class_name.php";
+<?php
+function require_navbar() {
+    require_once 'includes/navbar.php';
 }
+
+function link_bootstrap_style() {
+    echo '
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    ';
+}
+
+function link_bootstrap_script() {
+    echo '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>';
+}
+
+function auto_loader($class_name) {
+    if (str_contains($class_name, 'Table'))
+        require_once "includes/classes/$class_name.php";
+    else
+        require_once "includes/$class_name.php";
+}
+spl_autoload_register('auto_loader');
 spl_autoload_register('auto_loader');
